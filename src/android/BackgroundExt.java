71ed39506290059a73d32b26c1f6cbd6ed4c8208
JOOBA <33761785+joobasas@users.noreply.github.com>
2019-06-06 21:34:23 +0100

    Update and rename BackgroundModeExt.java to BackgroundExt.java

--- src/android/BackgroundExt.java
+++ src/android/BackgroundExt.java
@@ -1,22 +1,22 @@
 /*
- Copyright 2013 Sebastián Katzer
+    Copyright 2013-2017 appPlant GmbH
 
- Licensed to the Apache Software Foundation (ASF) under one
- or more contributor license agreements.  See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership.  The ASF licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
 
- http://www.apache.org/licenses/LICENSE-2.0
+     http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
  */
 
 package de.appplant.cordova.plugin.background;
@@ -26,32 +26,24 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.AppTask;
-import android.app.AlertDialog;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.PowerManager;
 import android.view.View;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
-import java.util.Arrays;
 import java.util.List;
 
-import static android.R.string.cancel;
-import static android.R.string.ok;
-import static android.R.style.Theme_DeviceDefault_Light_Dialog;
 import static android.content.Context.ACTIVITY_SERVICE;
 import static android.content.Context.POWER_SERVICE;
-import static android.content.pm.PackageManager.MATCH_DEFAULT_ONLY;
 import static android.os.Build.VERSION.SDK_INT;
 import static android.os.Build.VERSION_CODES.M;
 import static android.provider.Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS;
@@ -64,37 +56,56 @@ import static android.view.WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON;
  * Implements extended functions around the main purpose
  * of infinite execution in the background.
  */
-public class BackgroundModeExt extends CordovaPlugin {
+class BackgroundExt {
+
+    // Reference to the cordova interface passed by the plugin
+    private final CordovaInterface cordova;
+
+    // Reference to the cordova web view passed by the plugin
+    private final CordovaWebView webView;
 
     // To keep the device awake
     private PowerManager.WakeLock wakeLock;
 
     /**
+     * Initialize the extension to perform non-background related tasks.
+     *
+     * @param plugin The cordova plugin.
+     */
+    BackgroundExt(CordovaPlugin plugin)
+    {
+        this.cordova = plugin.cordova;
+        this.webView = plugin.webView;
+    }
+
+    /**
+     * Executes the request within a thread.
+     *
+     * @param action   The action to execute.
+     * @param callback The callback context used when
+     *                 calling back into JavaScript.
+     */
+    void executeAsync (String action, CallbackContext callback)
+    {
+        cordova.getThreadPool().execute(() -> execute(action, callback));
+    }
+
+    /**
      * Executes the request.
      *
      * @param action   The action to execute.
-     * @param args     The exec() arguments.
      * @param callback The callback context used when
      *                 calling back into JavaScript.
-     *
-     * @return Returning false results in a "MethodNotFound" error.
      */
-    @Override
-    public boolean execute (String action, JSONArray args,
-                            CallbackContext callback)
+    private void execute (String action, CallbackContext callback)
     {
-        boolean validAction = true;
-
         switch (action)
         {
-            case "battery":
+            case "batteryoptimizations":
                 disableBatteryOptimizations();
                 break;
-            case "webview":
+            case "webviewoptimizations":
                 disableWebViewOptimizations();
-                break;
-            case "appstart":
-                openAppStart(args.opt(0));
                 break;
             case "background":
                 moveToBackground();
@@ -115,17 +126,7 @@ public class BackgroundModeExt extends CordovaPlugin {
                 wakeup();
                 unlock();
                 break;
-            default:
-                validAction = false;
         }
-
-        if (validAction) {
-            callback.success();
-        } else {
-            callback.error("Invalid action: " + action);
-        }
-
-        return validAction;
     }
 
     /**
@@ -153,7 +154,7 @@ public class BackgroundModeExt extends CordovaPlugin {
                 Intent.FLAG_ACTIVITY_SINGLE_TOP |
                 Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-        clearScreenAndKeyguardFlags();
+        clearSreenAndKeyguardFlags();
         app.startActivity(intent);
     }
 
@@ -185,10 +186,6 @@ public class BackgroundModeExt extends CordovaPlugin {
         thread.start();
     }
 
-    /**
-     * Disables battery optimizations for the app.
-     * Requires permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS to function.
-     */
     @SuppressLint("BatteryLife")
     private void disableBatteryOptimizations()
     {
@@ -207,58 +204,6 @@ public class BackgroundModeExt extends CordovaPlugin {
         intent.setData(Uri.parse("package:" + pkgName));
 
         cordova.getActivity().startActivity(intent);
-    }
-
-    /**
-     * Opens the system settings dialog where the user can tweak or turn off any
-     * custom app start settings added by the manufacturer if available.
-     *
-     * @param arg Text and title for the dialog or false to skip the dialog.
-     */
-    private void openAppStart (Object arg)
-    {
-        Activity activity = cordova.getActivity();
-        PackageManager pm = activity.getPackageManager();
-
-        for (Intent intent : getAppStartIntents())
-        {
-            if (pm.resolveActivity(intent, MATCH_DEFAULT_ONLY) != null)
-            {
-                JSONObject spec = (arg instanceof JSONObject) ? (JSONObject) arg : null;
-
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-                if (arg instanceof Boolean && !((Boolean) arg))
-                {
-                    activity.startActivity(intent);
-                    break;
-                }
-
-                AlertDialog.Builder dialog = new AlertDialog.Builder(activity, Theme_DeviceDefault_Light_Dialog);
-
-                dialog.setPositiveButton(ok, (o, d) -> activity.startActivity(intent));
-                dialog.setNegativeButton(cancel, (o, d) -> {});
-                dialog.setCancelable(true);
-
-                if (spec != null && spec.has("title"))
-                {
-                    dialog.setTitle(spec.optString("title"));
-                }
-
-                if (spec != null && spec.has("text"))
-                {
-                    dialog.setMessage(spec.optString("text"));
-                }
-                else
-                {
-                    dialog.setMessage("missing text");
-                }
-
-                activity.runOnUiThread(dialog::show);
-
-                break;
-            }
-        }
     }
 
     /**
@@ -374,7 +319,7 @@ public class BackgroundModeExt extends CordovaPlugin {
     /**
      * Clears required flags to the window to unlock/wakeup the device.
      */
-    private void clearScreenAndKeyguardFlags()
+    private void clearSreenAndKeyguardFlags()
     {
         getApp().runOnUiThread(() -> getApp().getWindow().clearFlags(FLAG_ALLOW_LOCK_WHILE_SCREEN_ON | FLAG_SHOW_WHEN_LOCKED | FLAG_TURN_SCREEN_ON | FLAG_DISMISS_KEYGUARD));
     }
@@ -413,33 +358,5 @@ public class BackgroundModeExt extends CordovaPlugin {
     private Object getService(String name)
     {
         return getApp().getSystemService(name);
-    }
-
-    /**
-     * Returns list of all possible intents to present the app start settings.
-     */
-    private List<Intent> getAppStartIntents()
-    {
-        return Arrays.asList(
-            new Intent().setComponent(new ComponentName("com.miui.securitycenter","com.miui.permcenter.autostart.AutoStartManagementActivity")),
-            new Intent().setComponent(new ComponentName("com.letv.android.letvsafe", "com.letv.android.letvsafe.AutobootManageActivity")),
-            new Intent().setComponent(new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.appcontrol.activity.StartupAppControlActivity")),
-            new Intent().setComponent(new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity")),
-            new Intent().setComponent(new ComponentName("com.coloros.safecenter", "com.coloros.safecenter.permission.startup.StartupAppListActivity")),
-            new Intent().setComponent(new ComponentName("com.coloros.safecenter", "com.coloros.safecenter.startupapp.StartupAppListActivity")),
-            new Intent().setComponent(new ComponentName("com.oppo.safe", "com.oppo.safe.permission.startup.StartupAppListActivity")),
-            new Intent().setComponent(new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.AddWhiteListActivity")),
-            new Intent().setComponent(new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.BgStartUpManager")),
-            new Intent().setComponent(new ComponentName("com.vivo.permissionmanager", "com.vivo.permissionmanager.activity.BgStartUpManagerActivity")),
-            new Intent().setComponent(new ComponentName("com.asus.mobilemanager", "com.asus.mobilemanager.autostart.AutoStartActivity")),
-            new Intent().setComponent(new ComponentName("com.asus.mobilemanager", "com.asus.mobilemanager.entry.FunctionActivity")).setData(android.net.Uri.parse("mobilemanager://function/entry/AutoStart")),
-            new Intent().setAction("com.letv.android.permissionautoboot"),
-            new Intent().setComponent(new ComponentName("com.samsung.android.sm_cn", "com.samsung.android.sm.ui.ram.AutoRunActivity")),
-            new Intent().setComponent(ComponentName.unflattenFromString("com.iqoo.secure/.MainActivity")),
-            new Intent().setComponent(ComponentName.unflattenFromString("com.meizu.safe/.permission.SmartBGActivity")),
-            new Intent().setComponent(new ComponentName("com.yulong.android.coolsafe", ".ui.activity.autorun.AutoRunListActivity")),
-            new Intent().setComponent(new ComponentName("cn.nubia.security2", "cn.nubia.security.appmanage.selfstart.ui.SelfStartActivity")),
-            new Intent().setComponent(new ComponentName("com.zui.safecenter", "com.lenovo.safecenter.MainTab.LeSafeMainActivity"))
-        );
     }
 }
