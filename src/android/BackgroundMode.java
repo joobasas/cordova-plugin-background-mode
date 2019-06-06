312365af7a574a585fc16d9db3a35e8a4df6c608
JOOBA <33761785+joobasas@users.noreply.github.com>
2019-06-06 21:33:42 +0100

    Update BackgroundMode.java

--- src/android/BackgroundMode.java
+++ src/android/BackgroundMode.java
@@ -1,22 +1,22 @@
 /*
- Copyright 2013 Sebastián Katzer
+    Copyright 2013-2017 appPlant GmbH
 
- Licensed to the Apache Software Foundation (ASF) under one
- or more contributor license agreements.  See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership.  The ASF licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
 
- http://www.apache.org/licenses/LICENSE-2.0
+     http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
  */
 
 package de.appplant.cordova.plugin.background;
@@ -35,7 +35,7 @@ import org.json.JSONObject;
 import de.appplant.cordova.plugin.background.ForegroundService.ForegroundBinder;
 
 import static android.content.Context.BIND_AUTO_CREATE;
-import static de.appplant.cordova.plugin.background.BackgroundModeExt.clearKeyguardFlags;
+import static de.appplant.cordova.plugin.background.BackgroundExt.clearKeyguardFlags;
 
 public class BackgroundMode extends CordovaPlugin {
 
@@ -103,6 +103,16 @@ public class BackgroundMode extends CordovaPlugin {
                 break;
             case "disable":
                 disableMode();
+                break;
+            case "webviewoptimizations":
+            case "batteryoptimizations":
+            case "background":
+            case "foreground":
+            case "tasklist":
+            case "dimmed":
+            case "wakeup":
+            case "unlock":
+                new BackgroundExt(this).executeAsync(action, callback);
                 break;
             default:
                 validAction = false;
@@ -284,7 +294,7 @@ public class BackgroundMode extends CordovaPlugin {
         String str = String.format("%s._setActive(%b)",
                 JS_NAMESPACE, active);
 
-        str = String.format("%s;%s.on%s(%s)",
+        str = String.format("%s;%s.on('%s', %s)",
                 str, JS_NAMESPACE, eventName, params);
 
         str = String.format("%s;%s.fireEvent('%s',%s);",
